cd573667129ce6f05dc7cbacc70d5905fe880f17
Bruno Lima <brunojet@gmail.com>
2024-11-17 21:51:15 -0300

    Log INFO

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
         reporters: ['progress', 'kjhtml', 'coverage'],
         port: 9876,
         colors: true,
-        logLevel: config.LOG_DEBUG,
+        logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['ChromeHeadless'],
         customLaunchers: {
