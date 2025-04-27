1ea06a0f4ca22125c61772b3c5927ad6dc39d157
Bruno Lima <brunojet@gmail.com>
2025-04-27 20:50:16 -0300

    Vence o termo em um min

--- src/app/services/terms.service.ts
+++ src/app/services/terms.service.ts
@@ -5,14 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class TermsService {
   private readonly TERMS_KEY = 'termsAccepted';
+  private readonly TERMS_EXPIRATION_KEY = 'termsExpiration';
   private redirectUrl: string | null = null;
 
   areTermsAccepted(): boolean {
-    return localStorage.getItem(this.TERMS_KEY) === 'true';
+    const expiration = localStorage.getItem(this.TERMS_EXPIRATION_KEY);
+    if (expiration && new Date().getTime() < parseInt(expiration, 10)) {
+      return true;
+    }
+    return false;
   }
 
   acceptTerms(): void {
-    localStorage.setItem(this.TERMS_KEY, 'true');
+    const expirationTime = new Date().getTime() + 60 * 1000; // 60 seconds from now
+    localStorage.setItem(this.TERMS_EXPIRATION_KEY, expirationTime.toString());
   }
 
   setRedirectUrl(url: string): void {
